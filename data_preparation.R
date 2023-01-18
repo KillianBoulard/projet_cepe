4f735dddad8952a5104a752512e90ee882b5c68d
Killian Boulard <killian.boulard@gmail.com>
2023-01-18 22:48:54 +0100

    processing

--- data_preparation.R
+++ data_preparation.R
@@ -2,13 +2,16 @@
 #Emplacement local des fichiers (à définir pour chaque utilisateur)
 setwd("c:/Users/User/Desktop/Cours et documents/formation R ensae/DATA")
 
-# Le dataset incidendies comprend l'ensemble des incendies jour / jour 
+# Le dataset incendies comprend l'ensemble des incendies jour / jour 
 # Jusqu'à la maille commune du 01/01/2010 au 31/12/2021
-icendies <- read.csv(file="Incendies.csv", header = T, sep=";", skip = 6)
+incendies <- read.csv(file="Incendies.csv", header = T, sep=";", skip = 6)
 
 #Le dataset meteo comprend l'ensemble des données métérologiques de 
 # 2010 à 2023
 meteo <- read.csv(file="donnees-synop-essentielles-omm.csv", header = T, sep=";")
 
+#Date processing en tibble
+as_tibble(incendies)
+as_tibble(meteo)
 
-format(d$date, format = "%m")
+
