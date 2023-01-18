784032b471500e50214d62896cb1865249db00b7
Killian Boulard <killian.boulard@gmail.com>
2023-01-18 22:21:51 +0100

    DATA_PREP

--- data_preparation.R
+++ data_preparation.R
@@ -1,1 +1,8 @@
-i = 2
+#Les fichiers sont disponibles dans README.md
+#Emplacement local des fichiers (à définir pour chaque utilisateur)
+setwd("c:/Users/User/Desktop/Cours et documents/formation R ensae/DATA")
+
+# Le dataset incidendies comprend l'ensemble des incendies jour / jour 
+# Jusqu'à la maille commune du 01/01/2010 au 31/12/2021
+icendies <- read.csv(file="Incendies.csv", header = T, sep=";", skip = 6)
+
