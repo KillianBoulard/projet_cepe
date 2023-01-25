305fb3284bdfbbfa4fe9c25d3b5a8abb6b4ea048
Killian Boulard <killian.boulard@gmail.com>
2023-01-25 17:33:01 +0100

    com

--- data_preparation.R
+++ data_preparation.R
@@ -65,32 +65,55 @@ test_incendies = incendies %>%
     v_moyenn_vent = as.double(v_moyenn_vent, na.rm= T),
     date_m_1 = as.Date(date_alerte, format = "%d/%m/%Y") %m+% months(-1),
     date_m_12 = as.Date(date_alerte, format = "%d/%m/%Y") %m+% months(-12),
+    
     annee = format(date_alerte," %Y"),
     mois = format(date_alerte," %m"),
     annee_m_1 = format(date_m_1, "%Y"),
     mois_m_1 = format(date_m_1," %m"),
     annee_m_12 = format(date_m_12, "%Y"),
     mois_m_12 = format(date_m_12," %m")) %>%
+  
   group_by(
     annee, mois, code_insee) %>%
+  
   mutate(
     occurence_commune_mois = n(),
     surface_parcourue = sum(surface_parcourue),
-    nb_origine_indetermine = sum(origine_alerte=="Indéterminé", na.rm=TRUE),
-    nb_origine_autre = sum(origine_alerte=="Autre", na.rm=TRUE),
-    nb_origine_moy_aer = sum(origine_alerte=="Moyen aérien", na.rm=TRUE),
-    nb_origine_patrouille = sum(origine_alerte=="Patrouille", na.rm=TRUE),
-    nb_origine_pomp_pre_pos = sum(origine_alerte=="Pompiers pré-positionnés", na.rm=TRUE),
-    nb_origine_cam = sum(origine_alerte=="Vigie-caméra", na.rm=TRUE)
+    
+    nb_origine_indetermine = sum(origine_alerte == "Indéterminé") / n(),
+    nb_origine_autre = sum(origine_alerte=="Autre", na.rm=TRUE) / n(),
+    nb_origine_moy_aer = sum(origine_alerte=="Moyen aérien", na.rm=TRUE) / n(),
+    nb_origine_patrouille = sum(origine_alerte=="Patrouille", na.rm=TRUE) / n(),
+    nb_origine_population = sum(origine_alerte=="Population", na.rm=TRUE) / n(),
+    nb_origine_pomp_pre_pos = sum(origine_alerte=="Pompiers pré-positionnés", na.rm=TRUE) / n(),
+    nb_origine_cam = sum(origine_alerte=="Vigie-caméra", na.rm=TRUE) / n(),
+    
+    nb_nat_accidentelle = sum(nature == "Accidentelle"),
+    nb_nat_inv_part = sum(nature == "Involontaire (particulier)"),
+    nb_nat_inv_trav = sum(nature == "Involontaire (travaux)"),
+    nb_nat_malv = sum(nature == "Malveillance"),
+    nb_nat_naturelle = sum(nature == "Naturelle")
+    
   ) %>%
   distinct(
     annee, mois, annee_m_1, mois_m_1, annee_m_12, mois_m_12, departement, nom_commune, code_insee,
-    surface_parcourue, occurence_commune_mois, nb_origine_indetermine, nb_origine_autre,
-    nb_origine_moy_aer, nb_origine_patrouille, nb_origine_pomp_pre_pos, nb_origine_cam) %>%
+    surface_parcourue, occurence_commune_mois, 
+    #Origine
+    nb_origine_indetermine, nb_origine_autre,
+    nb_origine_moy_aer, nb_origine_patrouille, nb_origine_pomp_pre_pos, 
+    nb_origine_cam, nb_origine_population,
+    #Nature
+    nb_nat_accidentelle, nb_nat_inv_part, nb_nat_inv_trav, nb_nat_malv, nb_nat_naturelle) %>%
+  
   select(
     annee, mois, annee_m_1, mois_m_1, annee_m_12, mois_m_12, departement, nom_commune, code_insee,
-    surface_parcourue, occurence_commune_mois, nb_origine_indetermine, nb_origine_autre,
-    nb_origine_moy_aer, nb_origine_patrouille, nb_origine_pomp_pre_pos, nb_origine_cam)
+    surface_parcourue, occurence_commune_mois,
+    #Origine
+    nb_origine_indetermine, nb_origine_autre,
+    nb_origine_moy_aer, nb_origine_patrouille, nb_origine_pomp_pre_pos,
+    nb_origine_cam, nb_origine_population,
+    #Nature
+    nb_nat_accidentelle, nb_nat_inv_part, nb_nat_inv_trav, nb_nat_malv, nb_nat_naturelle)
 
 
 #Ajout / jointure pour ajouter les variables concernant les mois M-1 et M-12
